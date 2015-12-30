d906ee3293fe44533505c8c2f36ac55a49b85305
Varun Chitre <varun.chitre15@gmail.com>
2015-12-30 08:54:10 -0500

    add thundercharge

--- drivers/power/mediatek/linear_charging.c
+++ drivers/power/mediatek/linear_charging.c
@@ -65,16 +65,17 @@
  ****************************************************************************/
 #include <linux/types.h>
 #include <linux/kernel.h>
-
 #include <mt-plat/battery_meter.h>
 #include <mt-plat/battery_common.h>
 #include <mt-plat/charging.h>
 #include <mach/mt_charging.h>
 #include <mt-plat/mt_boot.h>
+#ifdef CONFIG_THUNDERCHARGE_CONTROL
+#include "thundercharge_control.h"
+#endif
 #include <linux/delay.h>
 #include <linux/mutex.h>
 #include <linux/wakelock.h>
-
 
 
  /* ============================================================ // */
@@ -918,7 +919,36 @@ void select_charging_curret(void)
 			}
 #else
 			{
+
 				g_temp_CC_value = batt_cust_data.usb_charger_current;
+
+				g_temp_CC_value = batt_cust_data.usb_charger_current;
+#if defined(CONFIG_ARM64)
+				if (strncmp(CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE_NAMES,
+				    "k35v1_64_om_lwctg", 17) == 0)
+					g_temp_CC_value = batt_cust_data.ac_charger_current;
+
+				if (strncmp(CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE_NAMES,
+				    "k35v1_64_op01_lwctg", 19) == 0)
+					g_temp_CC_value = batt_cust_data.ac_charger_current;
+
+				if (strncmp(CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE_NAMES,
+				    "k35v1_64_om_lwctg_cnop", 22) == 0)
+					g_temp_CC_value = batt_cust_data.ac_charger_current;
+#elif defined(CONFIG_ARM)
+				if (strncmp(CONFIG_BUILD_ARM_APPENDED_DTB_IMAGE_NAMES,
+				    "k35v1_gmo_op02_qhd_512m", 23) == 0)
+					g_temp_CC_value = batt_cust_data.ac_charger_current;
+
+				if (strncmp(CONFIG_BUILD_ARM_APPENDED_DTB_IMAGE_NAMES,
+				    "k35v1_gmo_cnop_lwctg_512_35m", 28) == 0)
+					g_temp_CC_value = batt_cust_data.ac_charger_current;
+#ifdef CONFIG_THUNDERCHARGE_CONTROL
+				g_temp_CC_value_linear = custom_usb_current;
+#else
+				g_temp_CC_value_linear = cur_usb_charger;
+#endif
+
 			}
 #endif
 		} else if (BMT_status.charger_type == NONSTANDARD_CHARGER) {
@@ -928,6 +958,16 @@ void select_charging_curret(void)
 #if defined(CONFIG_MTK_PUMP_EXPRESS_SUPPORT)
 			if (is_ta_connect == KAL_TRUE && ta_vchr_tuning == KAL_TRUE)
 				g_temp_CC_value = CHARGE_CURRENT_1500_00_MA;
+#ifdef CONFIG_THUNDERCHARGE_CONTROL
+			g_temp_CC_value_linear = custom_ac_current;
+#else
+			g_temp_CC_value_linear = cur_no_std_charger;
+#endif
+		} else if (BMT_status.charger_type == STANDARD_CHARGER) {
+#ifdef CONFIG_THUNDERCHARGE_CONTROL
+			g_temp_CC_value_linear = custom_ac_current;
+#else
+			g_temp_CC_value_linear = cur_ac_charger;
 #endif
 		} else if (BMT_status.charger_type == CHARGING_HOST) {
 			g_temp_CC_value = batt_cust_data.charging_host_charger_current;
